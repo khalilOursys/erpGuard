6e5ecf01b00c714e38de406f5e8bc1a7955487e7
Rabie Msekni <rabie.msekni.oursys@gmail.com>
2025-10-05 19:31:47 +0100

    hosting update 1

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // Allow both frontend and backend
+    origin: ['http://localhost:3000', 'http://localhost:3001','https://www.oursys-dev.site'], // Allow both frontend and backend
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3001);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3001,'0.0.0.0');
   console.log('Server listening on', await app.getUrl());
 }
 bootstrap();
